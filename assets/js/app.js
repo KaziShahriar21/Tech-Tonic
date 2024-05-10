85fd5d936b40aa66be5ca262aa5a69754cb905c8
KaziShahriar21 <111010108+KaziShahriar21@users.noreply.github.com>
2024-05-11 05:30:51 +0600

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,19 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+var mainImg = document.getElementById("mainImg");
+
+var smallImg = document.getElementsByClassName("small-img"); 
+
+
+for(let i=0; i<5;i++)
+    {
+        smallImg[i].onclick = function(){
+            mainImg.src = smallImg[i].src;
+        }
+    }
+
+
+
+
